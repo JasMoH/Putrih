ef76fc6eda84bdc11005b08f3f7fe75d0e0052cb
jason <jmorelliharlan@gmail.com>
2018-05-02 14:19:16 -0400

    fixed off by one in point generation and face generation. can now produce 3 point "cylinder". still has problems with numSlices > 2

--- cylender.py
+++ cylender.py
@@ -4,7 +4,7 @@ import math
 from math import pi, sin, cos
 from numpy import linspace
 
-pointsInSlice = 30 #number of vertices in a given slice
+pointsInSlice = 3 #number of vertices in a given slice
 modelHeight = 1 #space between slices
 numSlices = 2 #total height of object in slices
 
@@ -20,37 +20,39 @@ def deletMeshes():
   # call the operator once
   bpy.ops.object.delete()
 
-def createVerts():
+def createCylinderVerts():
   #create a list of vertices
   verts = []
   currentSlice = 0
 
   #create a circle
   for z in linspace(0,modelHeight, numSlices):
-    for theta in linspace(0,2*pi,pointsInSlice):
+    for theta in linspace(0,2*pi,pointsInSlice,endpoint=False):
       verts.append([sin(theta),cos(theta),z])
 
 
   return verts
 
-def createFaces():
+def populateCylinder(faces):
   #create list of faces
-  faces = [];
 
   #the cylender defines a 2 dimensional manifold linked on 2 edges
   #the heights are zero indexed (hence numSlices - 1)
   #the faces are added "above" the current slice, as well as "bellow" the next slice
   #hence (numSlices -1) - 1
   for h in linspace(0,numSlices-2,numSlices-1): #iterate over height slices.
-    for r in range(0,pointsInSlice-1): #iterate over each point in slice
+    for r in range(0,pointsInSlice): #iterate over each point in slice
       if(r == pointsInSlice-1):
         #handle linking ends of manifold together
-        faces.append([r,r+pointsInSlice,pointsInSlice*h])
-        faces.append([r+pointsInSlice,pointsInSlice*h,pointsInSlice*(h+1)])
+        faces.append([r,r+pointsInSlice,int(pointsInSlice*h)])
+        faces.append([r+pointsInSlice,int(pointsInSlice*h),int(pointsInSlice*(h+1))])
       else:
         #handle rest of manifold
         faces.append([r,r+pointsInSlice,r+1])
         faces.append([r+pointsInSlice,r+1,r+1+pointsInSlice])
+
+    print("faces at " + str(h))
+    print(faces)
 
   return faces
 
@@ -82,7 +84,12 @@ def createMesh(verts,faces):
   bm.free()  # always do this when finished
 
 deletMeshes()
-verts = createVerts()
-faces = createFaces()
+verts = createCylinderVerts()
+faces = []
+faces = populateCylinder(faces)
+
+print("points")
+print(verts)
+print("faces")
 print(faces)
 createMesh(verts,faces)
